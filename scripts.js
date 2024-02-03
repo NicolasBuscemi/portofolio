cc3c49c6cc6284bd950bc30656d414c43224905e
NicolasBuscemi <91195492+NicolasBuscemi@users.noreply.github.com>
2024-02-03 14:07:59 +0100

    Light/NightToggle

--- scripts.js
+++ scripts.js
@@ -1,29 +1,38 @@
-document.querySelector('#home h1').addEventListener('mouseover', function () {
-    this.textContent = 'BUSCEMI';
-});
-
-document.querySelector('#home h1').addEventListener('mouseout', function () {
-    this.textContent = 'NICOLAS';
-});
-
-
-
-document.addEventListener('DOMContentLoaded', function () {
-const scrollLinks = document.querySelectorAll('.scroll-link');
-
-scrollLinks.forEach(function (link) {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-
-        if (targetElement) {
-            window.scrollTo({
-                top: targetElement.offsetTop,
-                behavior: 'smooth'
-            });
-        }
+(function () {
+    const homeH1 = document.querySelector('#home h1');
+    homeH1.addEventListener('mouseover', function () {
+        homeH1.textContent = 'BUSCEMI';
     });
-});
-});
+
+    homeH1.addEventListener('mouseout', function () {
+        homeH1.textContent = 'NICOLAS';
+    });
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const scrollLinks = document.querySelectorAll('.scroll-link');
+
+        scrollLinks.forEach(function (link) {
+            link.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                const targetId = this.getAttribute('href').substring(1);
+                const targetElement = document.getElementById(targetId);
+
+                if (targetElement) {
+                    window.scrollTo({
+                        top: targetElement.offsetTop,
+                        behavior: 'smooth'
+                    });
+                }
+            });
+        });
+    });
+})();
+
+function toggleNightMode() {
+    const body = document.body;
+    body.classList.toggle('night-mode');
+
+    const nightModeToggle = document.getElementById('nightModeToggle');
+    nightModeToggle.textContent = body.classList.contains('night-mode') ? 'LIGHT' : 'NIGHT';
+}
